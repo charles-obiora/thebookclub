4f7606cfa045103f77b58e2aceed1bc56f05ad38
Chukwuemelie <obioraemelie@gmail.com>
2024-11-16 12:19:11 +0000

    install the react-router-dom, create a pages folders and set up the AuthPage for the login and signup and setting up routing using the previously installed react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+
 function App() {
+  const router = createBrowserRouter([
+    { path: "/auth", element: <AuthPage /> },
+  ]);
   return (
-    <div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
